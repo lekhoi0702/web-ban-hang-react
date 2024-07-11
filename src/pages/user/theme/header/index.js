6133627f2f52c9373dea265f27d6c029a701582c
Le Tuan Khoi <93958255+lekhoi0702@users.noreply.github.com>
2024-07-11 15:22:30 +0700

    second comit

--- src/pages/user/theme/header/index.js
+++ src/pages/user/theme/header/index.js
@@ -1,36 +1,85 @@
 import { memo } from "react";
 import "./style.scss";
-import { AiFillFacebook } from "react-icons/ai";
+import { Link } from "react-router-dom";
+import { FaFacebook } from "react-icons/fa";
+import { FaPhoneAlt } from "react-icons/fa";
+import { FaShippingFast } from "react-icons/fa";
+import { CiLogin } from "react-icons/ci";
+import { FaInstagram } from "react-icons/fa";
+import { SiZalo } from "react-icons/si";
+import { IoHome } from "react-icons/io5";
+import { FaShoppingCart } from "react-icons/fa";
 const Header = () => {
   return (
     <>
       <div className="header_top">
         <div className="container">
           <div className="row">
-            <div className="col-3 header_top_left">
+            <div className="col-4 header_top_left">
               <ul>
                 <li>
-                  <img
-                    src="https://salt.tikicdn.com/ts/upload/0e/07/78/ee828743c9afa9792cf20d75995e134e.png"
-                    alt="logo"
-                  ></img>
+                  <Link>
+                    <FaFacebook></FaFacebook>
+                  </Link>
                 </li>
                 <li>
-                  <span>Rẻ hơn Shopee</span>
+                  <Link>
+                    <FaInstagram />
+                  </Link>
+                </li>
+                <li>
+                  <Link>
+                    <SiZalo />
+                  </Link>
+                </li>
+                <li>
+                  <FaPhoneAlt></FaPhoneAlt>
+                  <span>(+84)944458409</span>
                 </li>
               </ul>
             </div>
-            <div className="col-4">thanh tìm kiếm</div>
+            <div className="col-3 header_top_center">
+              <span>Miễn phí giao hàng toàn quốc</span>
+              <FaShippingFast></FaShippingFast>
+            </div>
             <div className="col-3 header_top_right">
               <ul>
-                <li>Trang chủ</li>
-                <li>Tài khoản</li>
-                <li>Giỏ hàng</li>
                 <li>
-                  <AiFillFacebook />
+                  <Link>
+                    <IoHome />
+                    <span>Trang chủ</span>
+                  </Link>
+                </li>
+                <li>
+                  <Link>
+                    <CiLogin />
+                    <span>Đăng nhập</span>
+                  </Link>
                 </li>
               </ul>
             </div>
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-4">
+            <img src="https://salt.tikicdn.com/ts/upload/0e/07/78/ee828743c9afa9792cf20d75995e134e.png"></img>
+          </div>
+          <div className="col-3">
+            <span>Chào mừng đến với KHÔI LAPTOP</span>
+          </div>
+          <div className="col-3 header_cart">
+            <ul>
+              <li>
+                <Link>
+                  <FaShoppingCart></FaShoppingCart>
+                </Link>
+              </li>
+              <li>
+                <span>1</span>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
